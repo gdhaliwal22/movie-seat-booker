ad557a0e8da05b4ddacceff7badffe595e71bd3a
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2020-07-20 15:44:58 -0700

    Movie selection updating # of tickets and price

--- script.js
+++ script.js
@@ -5,13 +5,30 @@ const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 
 //Added parseInt to change string to integer
-const ticketPrice = parseInt(movieSelect.value);
+let ticketPrice = parseInt(movieSelect.value);
 
+// Update total and count
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
+  const selectedSeatsCount = selectedSeats.length;
+
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// Movie select event
+movieSelect.addEventListener("change", (e) => {
+  ticketPrice = parseInt(e.target.value);
+  updateSelectedCount();
+});
+
+// Seat click event
 container.addEventListener("click", (e) => {
   if (
     e.target.classList.contains("seat") &&
     !e.target.classList.contains("occupied")
   ) {
     e.target.classList.toggle("selected");
+    updateSelectedCount();
   }
 });
